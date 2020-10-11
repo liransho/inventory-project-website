b6097bfd2cd4d8876f8c8ff8688a19343357a420
tzlilshtam <68240894+tzlilshtam@users.noreply.github.com>
2020-10-11 15:22:34 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,40 @@
+
+<?php
+
+echo "
+<header>
+    <div class=\"collapse bg-dark\" id=\"navbarHeader\">
+        <div class=\"container\">
+            <div class=\"row\">
+                <div class=\"col-sm-8 col-md-7 py-4\">
+                    <h4 class=\"text-white\">About</h4>
+                    <p class=\"text-muted\">Ami & Meir thank you for a great course we hope to get 100.</p>
+                </div>
+                <div class=\"col-sm-4 offset-md-1 py-4\">
+                    <h4 class=\"text-white\">Contact</h4>
+                    <ul class=\"list-unstyled\">
+                        <li class='text-white'>Tzlil Shtam</li>
+                        <li class='text-white'>Liran Shoshana</li>
+                        <li><a href=\"https://www.instagram.com/tzlil.s/\" class=\"text-white\">Follow on instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class=\"navbar navbar-inverse\">
+        <div class=\"container-fluid\">
+        <div class=\"navbar-header\">
+        <a class=\"navbar-brand\" href=\"#\">LTz</a>
+        </div>
+        <ul class=\"nav navbar-nav\">
+        <li class=\"active\"><a href=\"#\">Home</a></li>
+        </ul>
+        <div class='text-primary text-right margin-bottom' id=\"demo\"></div>
+        <div>           
+        <button id='hidebtn' class='glyphicon glyphicon-align-justify pull-right' data-toggle='collapse' data-target='#navbarHeader' role='button' aria-expanded='false' aria-controls='navbarHeader'></button>
+        </div>
+  </div> 
+ 
+    </nav>
+</header>
+";
