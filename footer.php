a2e8064f0dd25ead007c466fa958f2220041fd7b
tzlilshtam <68240894+tzlilshtam@users.noreply.github.com>
2020-10-11 15:24:11 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer class="navbar navbar-inverse">
+
+    <!-- Copyright -->
+    <div class=" text-primary footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://github.com/liransho/"> Tzlil Shtam & Liran Shoshana</a>
+        <br>
+        <a class="text-info" href="index.php">Back to Top</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
