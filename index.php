1012246f7b03a9b0b6ed921f1aa32fa0c4ca7c3c
tzlilshtam <68240894+tzlilshtam@users.noreply.github.com>
2020-10-11 15:21:09 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+  
+<?php
+include_once ('connect.php');
+include_once ('Product.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js.js"></script>
+<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' type='text/javascript'></script>
+
+</head>
+<body >
+<?php
+require_once ('header.php');
+require_once('mainPart.php') ;
+require_once ('footer.php');
+?>
+</body>
+</html>
